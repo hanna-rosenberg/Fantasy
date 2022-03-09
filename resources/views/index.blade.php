3399f4fcf3c96140a913e15a847d21730f95c5dd
patrosk <naiade99@gmail.com>
2022-03-09 14:03:08 +0100

    worked on index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,30 +3,18 @@
         height: 500px;
     }
 
+    button {
+        cursor: pointer;
+        border: none;
+        background-color: none;
+    }
+
     h1 {
         margin: auto;
     }
 
-    .hp-button {
-        width: 40%;
-        height: 100%;
-        background-position: center;
-        background-repeat: no-repeat;
-        background-size: cover;
-        background-image: url('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ffffmovieposters.com%2Fwp-content%2Fuploads%2F73931.jpg&f=1&nofb=1');
-        cursor: pointer;
-        border: none;
-    }
-
-    .lotr-button {
-        width: 40%;
-        height: 100%;
-        background-position: center;
-        background-repeat: no-repeat;
-        background-size: cover;
-        background-image: url('https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimg.goldposter.com%2F2015%2F04%2FThe-Lord-of-the-Rings-The-Fellowship-of-the-Ring_poster_goldposter_com_30.jpg&f=1&nofb=1');
-        cursor: pointer;
-        border: none;
+    .book-container {
+        display: flex;
     }
 </style>
 <?php
@@ -47,19 +35,13 @@ $characters = DB::select('select * from characters');
 ?>
 
 <h1>Who are you?</h1>
-@foreach($books as $book)
-{{$book->title}}
-{{$book->id}}
-{{$book->author}}
-{{$book->year}}
-<form action="">
-    <button>Choose</button>
-</form>
-@endforeach
-
-<form action="/hp" method="post">
-    <button class="hp-button" type="submit"></button>
-</form>
-<form>
-    <button class="lotr-button" type="submit"></button>
-</form>
+<div class="book-container">
+    @foreach($books as $book)
+    <div class="book">
+        {{$book->title}}
+        <form action="/book?id=<?= $book->id ?>" method="post">
+            <button> <img src="./images/books/<?= $book->img_url ?>" alt=""></button>
+        </form>
+    </div>
+    @endforeach
+</div>
