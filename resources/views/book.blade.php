3f574e843bfd2ff58edefd32f074e805dfedcde6
patrosk <naiade99@gmail.com>
2022-03-10 19:03:00 +0100

    added img and button to book view

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -1,1 +1,9 @@
 <h1>You have chosen {{$book->title}}!</h1>
+
+<img src="./images/books/{{$book->img_url}}" alt="">
+
+<form action="/quiz">
+    @csrf
+
+    <button type="submit">Take the Quiz!</button>
+</form>
