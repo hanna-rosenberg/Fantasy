fe2d903130cb5e563129efd272e8de608356b858
LinneaEriksson <Linnea-E@hotmail.com>
2022-03-14 20:14:09 +0100

    fixed css for gallery

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -1,18 +1,18 @@
 @include('header')
 @include('menu')
+<section class="index">
+    <p>First choose your book:</p>
+    <div class="book-container">
+        @foreach($books as $book)
+        <div class="book">
+            {{$book->title}}
 
-<p>First choose your book:</p>
-<div class="book-container">
-    @foreach($books as $book)
-    <div class="book">
-        {{$book->title}}
+            <form action="charactergallery?book_id={{ $book->id }}" method="post">
 
-        <form action="charactergallery?book_id={{ $book->id }}" method="post">
-
-            @csrf
-            <button> <img src="./images/books/<?= $book->img_url ?>" alt="Poster of the feature film <?= $book->title ?>"></button>
-        </form>
-    </div>
-    @endforeach
-
-    @include('footer')
+                @csrf
+                <button> <img src="./images/books/<?= $book->img_url ?>" alt="Poster of the feature film <?= $book->title ?>"></button>
+            </form>
+        </div>
+        @endforeach
+</section>
+@include('footer')
