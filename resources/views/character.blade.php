56cd4d29d2032e5ab14de223c4d5d06295a78ffc
LinneaEriksson <Linnea-E@hotmail.com>
2022-03-14 21:12:12 +0100

    Added styling

--- resources/views/character.blade.php
+++ resources/views/character.blade.php
@@ -3,19 +3,20 @@
 <!-- Här ska karaktärerna loopas men det funkar ej -->
 
 <section class="character-section">
-    <h2>You chose {{$answer}}!</h2>
+    <h2>You are {{$character->name}}!</h2>
 
     <img src="./images/characters/{{$character->img_url}}" alt="Portrait of {{$character->name}}">
 
-    <h3>You are {{$character->name}}!</h3>
-    <ul>
-        <li>Name: {{$character->name}}</li>
-        <li>Age: {{$character->age}}</li>
-        <li>Creature: {{$character->creature}}</li>
-        <li>Alignment: {{$character->alignment}}</li>
-        <li>Power: {{$character->power}}</li>
-        <li>Colour: {{$character->colour}}</li>
-    </ul>
+    <h3>You chose {{$answer}} therefore you are {{$character->name}}!</h3>
+    <p>
+        Name: {{$character->name}}<br>
+        Age: {{$character->age}}<br>
+        Creature: {{$character->creature}}<br>
+        Alignment: {{$character->alignment}}<br>
+        Power: {{$character->power}}
+    </p>
+    <!-- <li>Colour: {{$character->colour}}</li> -->
+
     <h4>Summary</h4>
     <p>{{$character->summary}}</p>
 </section>
