36ad1451ec2181dcffac43d2e5a72f138a41e20f
patrosk <naiade99@gmail.com>
2022-03-15 09:04:10 +0100

    added database seeder for books

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Book;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $books = [
+            new Book([
+                'author' => 'J.K Rowling',
+                'title' => 'Harry Potter and the Philosopher\'s Stone',
+                'year' => 1997,
+                'img_url' => 'harry-potter.jpeg',
+                'password' => 'alohomora',
+                'hint' => 'Unlocking spell',
+                'hint_url' => 'https://pottermore.fandom.com/wiki/Unlocking_Spell'
+            ]),
+
+            new Book([
+                'author' => 'J.R.R Tolkien',
+                'title' => 'The Lord of the Rings: The Fellowship of the Ring',
+                'year' => 1954,
+                'img_url' => 'lord-of-the-rings.jpeg',
+                'password' => 'mellon',
+                'hint' => 'Speak friend, and enter',
+                'hint_url' => 'https://lotr.fandom.com/wiki/Doors_of_Durin'
+            ]),
+        ];
+        foreach ($books as $book) {
+            $book->save();
+        }
     }
 }
